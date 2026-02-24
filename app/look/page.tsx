6a5cbb741e868274712e94bbcc494a7dc6b91dd0
Bymbatseren <Zenitsu55555555@gmail.com>
2026-02-24 20:30:31 +0800

    second try

--- app/look/page.tsx
+++
@@ -1,8 +0,0 @@
-import IDCardFlip from "@/components/idCardFlipModal";
-import IdentityCard from "@/components/IdentityCard";
-import Modal from "@/components/modal";
-
-export default function LookPage() {
-  return (
-   <Modal/>
-  )}
